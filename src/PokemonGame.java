a36992e92f9911c717502a9e06b21cd031cde2b2
matov <manuel.aguiar.tovar@gmail.com>
2021-08-05 12:38:09 +0100

    Initial commit

---
+++ src/PokemonGame.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class PokemonGame {
+
+	/**
+	 * Main
+	 * @param args - string with sequece of movements
+	 * 
+	 * Each movement is a letter N, S, E or O (in portuguese: norte, sul, este, oeste).
+	 */
+	public static void main(String[] args) {
+		
+		//TODO
+		//boolean stringIsValid = validateString(args[0]);
+		//take care exceptions invalid string
+		
+		//TODO
+		//Create PokemonMap
+		
+		
+		//TODO
+		//Run the sequence in the map
+		
+		//TODO
+		//print the number of pokemons catched
+	}
+
+}
