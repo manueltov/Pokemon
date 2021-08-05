9cdfe316d11eebcd279eb63ee51af609fdfc9016
matov <manuel.aguiar.tovar@gmail.com>
2021-08-05 12:52:21 +0100

    create PokemonMap

---
+++ src/PokemonMap.java
@@ -0,0 +1,21 @@
+/**
+ * PokemonMap is the map for PokemonGame
+ * @author Manuel
+ *
+ */
+public class PokemonMap {
+
+	//Coordenadas posicao atual
+	//pontuacao atual
+	//set de posicoes por onde ja passou
+	//TODO
+	
+	//construtor (comeca na posicao 0,0)
+	//TODO
+	
+	//metodo move para outra posicao
+	//TODO
+	
+	//metodo devolve pontuacao
+	//TODO
+}
