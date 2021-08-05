579be3b5e88aef5ec93c95d871ec2cfa64cf0264
matov <manuel.aguiar.tovar@gmail.com>
2021-08-05 12:56:27 +0100

    create Coordinates

---
+++ src/Coordinates.java
@@ -0,0 +1,35 @@
+/**
+ * Coordinates x and y of a referencial
+ * @author Manuel
+ *
+ */
+public class Coordinates {
+
+	private int x;
+	private int y;
+	
+	public Coordinates(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	//criar method equals que vai ser preciso
+	//TODO
+	
+}
